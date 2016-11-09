a522628e3261ebb4dae7f401f7aeaa98be4cd19c
Julian NG <juliannieb@gmail.com>
2016-11-09 13:42:25 -0600

    Add functions to calculate B and g

--- final_proyect.cpp
+++ final_proyect.cpp
@@ -4,6 +4,15 @@
 
 using namespace std;
 
+double calculate_b(double m) {
+	return 1 - m;
+}
+
+double calculate_g(double m, double t, double k, double nh) {
+	double g = 2 * calculate_b(m) * nh * t * (1 - ((calculate_b(m) * nh * t) / k));
+	return g;
+}
+
 int main() {
 	cout << "Hola" << endl;
 	return 0;
